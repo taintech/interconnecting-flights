a89a4a77165d77f93b9d31b4fd9672383dc4cbe6
taintech <rinattainov@gmail.com>
2017-02-13 18:44:59 +0100

    Configured as deployable war

--- interconnecting-flights/src/main/java/com/taintech/interconnflights/Application.java
+++ interconnecting-flights/src/main/java/com/taintech/interconnflights/Application.java
@@ -2,13 +2,20 @@ package com.taintech.interconnflights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Author: Rinat Tainov
  * Date: 12/02/2017
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
